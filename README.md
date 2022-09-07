9db048e8ceba67a69ce0535dc9ae168f1e55a218
HhmedHesham <heshama7793@gmail.com>
2022-09-07 07:33:39 +0200

    readme modified and added a bucket with explicit dependencies to the vm

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # tf-cmd
 terraform useful commands and scripts
+
+# terraform init
+to intialize your new terrafrom configuration file
+
+# terraform apply
+to apply the changes to your infrastructure
+
+# terraform destroy
+to destroy the changes to your infrastructure
+
+# terraform plan
+to plan the changes to your infrastructure
+
+# terraform show
+to show the changes to your infrastructure
+
+# terraform validate
+to validate the changes to your infrastructure
+
+# terraform fmt
+to format the changes to your infrastructure
+
+# terraform taint
+to taint the changes to your infrastructure
+
+# terraform state
+to state the changes to your infrastructure
+
+there is a difference between explicit and implicit dependencies in terraform
+- the explicit dependencies are the dependencies that are defined in the terraform configuration file 
+e.g create a resource and then create another resource that depends on the first resource
+
+- the implicit dependencies are the dependencies that are not defined in the terraform configuration file 
+e.g create a resource and then create another resource that depends on the first resource but the second resource is not defined in the terraform configuration file
--- infra-as-code.tf
+++ infra-as-code.tf
@@ -1,5 +1,5 @@
 
-
+// required to know which provider to download and use for this project from the terraform registry
 terraform {
   required_providers {
     google = {
@@ -8,6 +8,7 @@ terraform {
   }
 }
 
+//  used to configure the named provider, in this case google u can use mutlipe progiders like aws as well 
 provider "google" {
   version = "3.5.0"
   project = "<PROJECT_ID>"
@@ -18,4 +19,65 @@ provider "google" {
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+    // name of the instance
+  name         = "terraform-instance"
+    // machine type of the instance
+  machine_type = "f1-micro"
+    // the zone in which the instance will be created
+    zone         = "us-central1-c"
+
+    tags         = ["web", "dev"]
+
+    // the disk image to use for the instance
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11" // or "cos-cloud/cos-stable"
+    }
+  }
+    // the network to which the instance will be connected
+  network_interface {
+    network = google_compute_network.vpc_network.name // the name of the network to which the instance will be connected
+    access_config {
+        nat_ip = google_compute_address.ip_address.address // the static ip address to be assigned to the instance
+    }
+  }
+}
+
+ // to create a static ip address 
+resource "google_compute_address" "ip_address" {
+  name = "terraform-static-ip"
+}
+
+# ------------------------------------------------------------------- #
+
+# New resource for the storage bucket our application will use.
+resource "google_storage_bucket" "example_bucket" {
+  name     = "<UNIQUE-BUCKET-NAME>"
+  location = "US"
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+# Create a new instance that uses the bucket
+resource "google_compute_instance" "another_instance" {
+  # Tells Terraform that this VM instance must be created only after the
+  # storage bucket has been created.
+  depends_on = [google_storage_bucket.example_bucket]
+  name         = "terraform-instance-2"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
 }
